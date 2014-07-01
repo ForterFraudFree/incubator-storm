a2e2ab8d99c00e21cc95add674bd8e1a3bc90331
Anya Tchernishov <anya@forter.com>
2014-07-01 15:02:18 +0300

    Add documentation to random int method

--- examples/storm-starter/multilang/resources/randomsentence.js
+++ examples/storm-starter/multilang/resources/randomsentence.js
@@ -54,6 +54,10 @@ RandomSentenceSpout.prototype.fail = function(id, done) {
     done();
 }
 
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
