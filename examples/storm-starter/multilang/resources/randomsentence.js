d6a659e795c05ba4f5297c3aa21ab61963137a78
Anya Tchernishov <anya@forter.com>
2014-07-03 10:06:05 +0300

    Fix documentation

--- examples/storm-starter/multilang/resources/randomsentence.js
+++ examples/storm-starter/multilang/resources/randomsentence.js
@@ -54,7 +54,6 @@ RandomSentenceSpout.prototype.fail = function(id, done) {
 
 /**
  * Returns a random integer between min (inclusive) and max (inclusive)
- * Using Math.round() will give you a non-uniform distribution!
  */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
